9bb37b9ea78d3d83a9fd447a437286a35d14b66f
Yurii Rashkovskii <yrashk@gmail.com>
2013-06-22 11:02:11 +0100

    Add more tests for DBI.Result

--- test/result_test.exs
+++ test/result_test.exs
@@ -6,4 +6,32 @@ defmodule DBI.ResultTest do
   test "result column index" do
     assert DBI.Result.new(columns: ["col1", "col2", "col3"]).index("col2") == 1
   end
+
+  test "zip the entire result" do
+    result = DBI.Result.new(columns: ["col1", "col2", "col3"],
+                            rows: [{"r1", "r1", "r1"},
+                                   {"r2", "r2", "r2"},
+                                   {"r3", "r3", "r3"},
+                                  ])
+    assert result.zip == [[{"col1", "r1"}, {"col2", "r1"}, {"col3", "r1"}],
+                          [{"col1", "r2"}, {"col2", "r2"}, {"col3", "r2"}],
+                          [{"col1", "r3"}, {"col2", "r3"}, {"col3", "r3"}],
+    ]
+  end
+
+  test "zip a row" do
+    result = DBI.Result.new(columns: ["col1", "col2", "col3"])
+    assert result.zip({"r1", "r1", "r1"}) == [{"col1", "r1"}, {"col2", "r1"}, {"col3", "r1"}]
+  end
+
+  test "result should be an enumerable" do
+    rows = [{"r1", "r1", "r1"},
+            {"r2", "r2", "r2"},
+            {"r3", "r3", "r3"},
+    ]
+    result = DBI.Result.new(columns: ["col1", "col2", "col3"],
+                            rows: rows)
+    assert Enum.to_list(result) == rows
+  end
+
 end
