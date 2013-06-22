07b4b5a524ad24e2ba8c1f9c823b6c6159019d5a
Yurii Rashkovskii <yrashk@gmail.com>
2013-06-22 11:04:54 +0100

    Minor source code prettifying

--- test/exdbi_test.exs
+++ test/exdbi_test.exs
@@ -5,6 +5,6 @@ defmodule DBITest do
 
   test "parse statement with placeholders" do
     assert DBI.Statement.parse("SELECT x FROM y WHERE a = :{a} AND b = :{b} AND c = :{a} AND d = :{A}") ==
-      ["SELECT x FROM y WHERE a = ", :a ," AND b = ", :b, " AND c = ", :a, " AND d = ", :A]
+      ["SELECT x FROM y WHERE a = ", :a, " AND b = ", :b, " AND c = ", :a, " AND d = ", :A]
   end
 end
