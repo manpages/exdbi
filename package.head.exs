57b626481fd034f4948c0473f0cc8cf878442b5b
Yurii Rashkovskii <yrashk@gmail.com>
2013-06-22 18:53:50 +0100

    Add expm package

---
+++ package.head.exs
@@ -0,0 +1,5 @@
+Expm.Package.new(name: "exdbi", description: "Elixir Database Interface",
+                 version: :head, keywords: ["database","db","sql"],
+                 maintainers: [[name: "Yurii Rashkovskii",
+                                email: "yrashk@gmail.com"]],
+                 repositories: [[github: "exdbi/exdbi"]])
