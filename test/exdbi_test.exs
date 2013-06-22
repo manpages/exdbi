6de4356bb6e60605c9875c48e896c3aa74093e36
Yurii Rashkovskii <yrashk@gmail.com>
2013-06-22 11:03:41 +0100

    Improve placeholder parsing test

--- test/exdbi_test.exs
+++ test/exdbi_test.exs
@@ -4,7 +4,7 @@ defmodule DBITest do
   use ExUnit.Case
 
   test "parse statement with placeholders" do
-    assert DBI.Statement.parse("SELECT x FROM y WHERE a = :{a} AND b = :{b} AND c = :{a}") == 
-      ["SELECT x FROM y WHERE a = ", :a ," AND b = ", :b, " AND c = ", :a]
+    assert DBI.Statement.parse("SELECT x FROM y WHERE a = :{a} AND b = :{b} AND c = :{a} AND d = :{A}") ==
+      ["SELECT x FROM y WHERE a = ", :a ," AND b = ", :b, " AND c = ", :a, " AND d = ", :A]
   end
 end
